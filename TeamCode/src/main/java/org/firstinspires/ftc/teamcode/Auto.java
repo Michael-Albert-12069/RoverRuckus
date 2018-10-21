6f5874b9b45ede0e0b72a7e48f54c4ec2adb5c25
Daniel-Perry-J <44074146+Daniel-Perry-J@users.noreply.github.com>
2018-10-21 11:59:53 -0700

    We done did good

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+
+@Autonomous(name = "Michael - Auto A", group = "Autonomous")
+
+public class Auto extends LinearOpMode {
+    private DcMotor Whatisleft;
+    private DcMotor Therightway;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        Whatisleft=hardwareMap.dcMotor.get("left");
+        Therightway=hardwareMap.dcMotor.get("left");
+        Whatisleft.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
+
+        double drivePower=-0.5;
+        double ZEROSPEED=0.00;
+
+        Forward(1,1000);
+
+
+    }
+    public void Forward(double power, long time)throws InterruptedException {
+        Whatisleft.setPower(power);
+        Therightway.setPower(power);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,4 +1,32 @@
+// created by Daniel Perry on 10/21/2018 //
+
 package org.firstinspires.ftc.teamcode;
 
-public class Teleop {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+@TeleOp(name = "Danny - TeleOp A", group = "TeleOp")
+public class Teleop extends LinearOpMode {
+    private DcMotor Whatisleft;
+    private DcMotor Therightway;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        Whatisleft=hardwareMap.dcMotor.get("left");
+        Therightway=hardwareMap.dcMotor.get("right");
+        Whatisleft.setDirection(DcMotor.Direction.REVERSE);
+
+
+        waitForStart();
+        while ((opModeIsActive())){
+            Whatisleft.setPower(-gamepad1.left_stick_y/10);
+            Therightway.setPower(-gamepad2.right_stick_y/10);
+
+            telemetry.update();
+            idle();
+        }
+
+    }
 }
